1002989086b14d3e390e3792e2f2d64bb5f4a5b8
nndukwe <eobiajuru@providusbank.com>
2025-04-04 01:22:22 +0100

    added a homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Yazid
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
